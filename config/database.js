460b94d5a4b8ab763ee9e2b121c1d800062934f5
umertaptap <umer.iftikhar@taptaptechnologies.com>
2025-06-10 20:14:13 +0500

    dev working

--- config/database.js
+++ config/database.js
@@ -1,7 +1,12 @@
 const path = require('path');
+const fs = require('fs');
+
+
+const cert = fs.readFileSync(`${__dirname}/../cert/ca-certificate.crt`).toString();
 
 module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'postgres');
+  
 
   const connections = {
     mysql: {
@@ -24,13 +29,13 @@ module.exports = ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
         schema: env('DATABASE_SCHEMA', 'public'),
+        ssl: true,
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
